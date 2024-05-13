a9b24174c4a790b28737682647a876a4e305e789
gurpreet1603 <gsardarji1995@gmail.com>
2024-05-13 22:09:32 +0530

    'main'

--- src/Schema/Blog.js
+++ src/Schema/Blog.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
-
 const department = new mongoose.Schema({
-  
   title: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId ,ref:"Users"},
   images: { type: String},
   description: { type: String},
-
 },{timestamps:true});
 
 module.exports = mongoose.model("Department", department);
