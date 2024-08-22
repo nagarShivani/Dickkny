5a42f8ec1fa3f4c98e6124ddd8add2712528746f
nagarShivani <nagarnanika@gmail.com>
2024-08-22 17:43:48 +0530

    port 80
    Port changes to 80 and Route

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const session = require('express-session');
-  const passport = require('passport');
-  const GoogleStrategy = require('passport-google-oauth20').Strategy;
-  require('./src/config/db'); 
-  const routes = require('./src/routes/routes'); 
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("./src/config/db");
+const routes = require("./src/routes/routes");
 
 const app = express();
 
-
 app.use(express.json());
-app.use( '*',cors());
+app.use("*", cors());
+app.get("/", (req, res) => {
+  res.status(200).json({
+    statusCode: 200,
+    message: "application is running..",
+  });
+});
+app.use("/", routes);
 
-app.use('/', routes);
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Your server is running on port ${port}`);
 });
