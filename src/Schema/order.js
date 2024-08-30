96f371e19dca07d5c822b2c63c861b5f8ca94c4b
nagarShivani <nagarnanika@gmail.com>
2024-08-30 14:03:41 +0530

    order enum

--- src/Schema/order.js
+++ src/Schema/order.js
@@ -1,45 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users',
-    required: true
-  },
-  products: [{
-    productId: {
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'product',
-
-      required: true
+      ref: "Users",
+      required: true,
     },
-    quantity: {
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "product",
+
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalPrice: {
       type: Number,
-      required: true
-    }
-  }],
-  totalPrice: {
-    type: Number,
-    required: true
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+    },
+    orderId: {
+      type: Number,
+    },
+    addressId: {
+      type: String,
+    },
+    paymentId: {
+      type: String,
+    },
+    status: {
+      type: String,
+      // enum: ['Pending', 'Processing', 'Shipped', 'Delivered'],
+      enum: [
+        "Pending",
+        "Transit",
+        "Out for Delivery",
+        "Completed",
+        "Return Requested",
+        "Return Received",
+      ],
+      default: "Pending",
+    },
   },
-  paymentMethod: {
-    type: String,
-  },
-  orderId: {
-    type: Number,
-  },
-  addressId: {
-    type: String,
-  },
-  paymentId: {
-    type: String,
-  },
-  status: {
-    type: String,
-    enum: ['Pending', 'Processing', 'Shipped', 'Delivered'],
-    default: 'Pending'
-  },
+  { timestamps: true }
+);
 
-}, { timestamps: true });
-
-module.exports = mongoose.model('Order', orderSchema);
+module.exports = mongoose.model("Order", orderSchema);
--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -1,21 +1,24 @@
-const Order = require('../Schema/order');
-const User = require('../Schema/userSchema');
-const imagesSchema = require('../Schema/imagesSchema');
-const multer = require('multer');
-const path = require('path');
+const Order = require("../Schema/order");
+const User = require("../Schema/userSchema");
+const imagesSchema = require("../Schema/imagesSchema");
+const multer = require("multer");
+const path = require("path");
 
 // Multer configuration for file storage
 exports.getAllOrders = async (req, res) => {
   try {
-    const orders = await Order.find().populate('products.productId').populate('userId').sort({ createdAt: -1 });;
-    if(!orders) {
-      return res.status(404).json({ error: 'No orders found' });
+    const orders = await Order.find()
+      .populate("products.productId")
+      .populate("userId")
+      .sort({ createdAt: -1 });
+    if (!orders) {
+      return res.status(404).json({ error: "No orders found" });
     }
 
     res.status(200).json({ orders });
   } catch (error) {
-    console.error('Error fetching orders:', error);
-    res.status(500).json({ error: 'Failed to fetch orders' });
+    console.error("Error fetching orders:", error);
+    res.status(500).json({ error: "Failed to fetch orders" });
   }
 };
 
@@ -24,17 +27,19 @@ exports.getMyOrders = async (req, res) => {
     const userId = req.params.userId; // Extract userId from request parameters
 
     // Find orders for the userId
-    const orders = await Order.find({ userId }).populate('products.productId').sort({ createdAt: -1 });;
+    const orders = await Order.find({ userId })
+      .populate("products.productId")
+      .sort({ createdAt: -1 });
 
     if (!orders || orders.length === 0) {
-      return res.status(404).json({ error: 'No orders found' });
+      return res.status(404).json({ error: "No orders found" });
     }
 
     // Find user record with matching userId
     const user = await User.findOne({ _id: userId });
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     // Create a map of user's addresses
@@ -44,38 +49,39 @@ exports.getMyOrders = async (req, res) => {
     }, {});
 
     // Attach the matched address to each product in the orders
-    const ordersWithAddressInProducts = orders.map(order => {
+    const ordersWithAddressInProducts = orders.map((order) => {
       const address = addressMap[order.addressId.toString()];
       return {
         ...order.toObject(),
-        products: order.products.map(product => ({
+        products: order.products.map((product) => ({
           ...product.toObject(),
-          selectedAddress: address
-        }))
+          selectedAddress: address,
+        })),
       };
     });
 
     // Send the updated orders
     res.status(200).json({ orders: ordersWithAddressInProducts });
   } catch (error) {
-    console.error('Error fetching orders or user:', error);
-    res.status(500).json({ error: 'Failed to fetch orders or user' });
+    console.error("Error fetching orders or user:", error);
+    res.status(500).json({ error: "Failed to fetch orders or user" });
   }
 };
 
-
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-  }
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
 });
 
-const upload = multer({ storage: storage }).single('image');
+const upload = multer({ storage: storage }).single("image");
 
 exports.upload = async (req, res) => {
   upload(req, res, async function (err) {
@@ -89,26 +95,26 @@ exports.upload = async (req, res) => {
     const image = req.file;
 
     if (!name || !image) {
-      return res.status(400).json({ error: 'Name and image are required' });
+      return res.status(400).json({ error: "Name and image are required" });
     }
 
     try {
       // Save the image details to MongoDB
       const newImage = new imagesSchema({
         image: image.path,
-        name: name
+        name: name,
       });
       await newImage.save();
 
       res.status(200).json({
-        message: 'Image uploaded and saved successfully',
+        message: "Image uploaded and saved successfully",
         data: {
           name: name,
-          imagePath: image.path
-        }
+          imagePath: image.path,
+        },
       });
     } catch (error) {
-      res.status(500).json({ error: 'Failed to save image to database' });
+      res.status(500).json({ error: "Failed to save image to database" });
     }
   });
 };
